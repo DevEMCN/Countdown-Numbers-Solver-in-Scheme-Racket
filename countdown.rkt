29fc95740fef5754e6783823c50eaf8393daf1a4
DevEMCN <devemcn@gmail.com>
2017-03-16 11:55:27 +0000

    reworked the validating rpn expression function, works correctly now

--- countdown.rkt
+++ countdown.rkt
@@ -33,58 +33,39 @@
 ;(define s (make-stack))
 ;(evalrpn l s)
 
-(define combs '((1 1 +)(4 2 5 * + 1 3 2 * + /)))
-;(define combs (combinations l))
-
+;(define combs '((1 1 +)(4 2 5 * + 1 3 2 * + /)))
+(define combs (combinations l))
 (define answer 2)
-;(define (find-answers l a) ; loop through the list of lists, a being a list that will contain the answers
-;  (let ((s (make-stack))) ; give the evalrpn a stack to use
-;    (if (null? l) a
-;  (cond
-;    [(null? (car l))(find-answers (cdr l) a)]
-;    [(= (car(evalrpn (car l) s)) answer) (cons (car l) a)] ; if the the evalrpn returns a number = to the answer, cons the list item
-;     ; if the list is now empty, return the answers list a othwise move onto the next item
-;    [else #f]))))
-
 (define a '())
-
-;(find-answers combs a)
 
 ; adpated from http://stackoverflow.com/questions/14506831/whats-the-fastest-way-to-check-if-input-string-is-a-correct-rpn-expression
 ; basically if this function does not return 1 then it is not valid rpn
 ; for example (3 4 + 1 *) count will start at 0 then goes 1-2-1-2-1 ending in 1, so this is valid
 ; (2 3 4 + 1) starts at 0 then goes 1-2-3-2-3 ending in 3, so this is not valid
+; or if it goes to 0 at any time inside the loop then it is not vali
 
-(define (validate-rpn lst count) ; function for validating if given list is valid rpn 
- (cond ((null? lst) ; if the list is empty return the count
-      count) ; return the stack
-      ((if (number? (car lst)) ; if the next item in the list is a number
-          (set! count (add1 count)) ; push that number onto the stack
-          (set! count (sub1 count))) ; return the stack
-      (validate-rpn (cdr lst) count)))) ;  back into the loop
+(define valence-opr -1)
+(define valence-opd 1)
+
+(define (validate-rpn l count) ;reworked version of count which is more functional looking and has the check to see if count goes below 0
+  (cond [(null? l) count]  ; if the list is empty, return the count
+        ;  ; otherwise, check of the next item in the list is a number and set the correct valence for count
+        [(if (number? (car l)) (set! count (+ count valence-opd))(set! count (+ count valence-opr)))
+         ; otherwise if the count is <= 0 the epression is invalid so return count which above will be filtered out
+         (if (<= count 0) count (validate-rpn (cdr l) count))])) ; otherwise keep looping around
 
 ;(validate-rpn l 0)
 
- ;(define perms (filter (lambda (x) (= (validate-rpn x 0) 1)) combs)) ;function filters valid rpn expressions
+(define perms (filter (lambda (x) (= (validate-rpn x 0) 1)) combs)) ;function filters valid rpn expressions
 
-
-(define (find-answers) ; loop through the list of lists, a being a list that will contain the answers
-  (let ((s (make-stack))) ; give the evalrpn a stack to use
-    (filter
-     (lambda (x)
-       (let ((an (evalrpn x s)))
-       (= (car an) answer))
-     )combs)))
-
-;(find-answers)
+perms
 
 (define (find-ans) ; loop through the list of lists, a being a list that will contain the answers
     (filter
      (lambda (x)
        (let ((s (make-stack))) ; give the evalrpn a stack to use
        (let ((an (evalrpn x s)))
-       (and (= (car an) answer) (< (length an) 2)))) ; the reason for finding the length of stack is that my count function has a slight bug in it. see note 3
-     )combs))
+       (= (car an) answer))) ; the reason for finding the length of stack is that my count function has a slight bug in it. see note 3
+     )perms))
 
-(find-ans)
-
+;(find-ans)
